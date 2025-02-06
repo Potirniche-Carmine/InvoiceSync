e762364b1eb154f6ceeded1b355c66d57bf50cc0
Carmine Potirniche <potirnichecarmine@gmail.com>
2025-02-05 21:08:16 -0800

    fixing

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  // Add these configurations
   images: {
     unoptimized: true
   },
   experimental: {
     outputFileTracingIncludes: {
-      '/app/fonts/**/*': true,  // For your local fonts
-      '/public/**/*': true      // For public assets
+      '/app/fonts/**/*': true, 
+      '/public/**/*': true    
     }
   }
 };
