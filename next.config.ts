70284aa034f905e442fda8909c1e1799f7cbcc47
Carmine Potirniche <potirnichecarmine@gmail.com>
2025-02-05 22:52:13 -0800

    reverting

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  images: {
-    unoptimized: true
-  },
-  experimental: {
-    outputFileTracingIncludes: {
-      '/app/fonts/**/*': true, 
-      '/public/**/*': true    
-    }
-  }
+  output: "standalone"
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,10 +17,7 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "Locksmith4U",
-  description: "A local locksmith service that serves all of the Las Vegas area.",
-  icons: {
-    icon: '/favicon.ico'
-  },
+  description: "A local locksmith service that serves all of the Las Vegas area."
 };
 
 export default function RootLayout({
@@ -30,9 +27,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialised flex flex-col min-h-screen bg-white text-black`}>
         <Header />
         <div className="flex-grow">
