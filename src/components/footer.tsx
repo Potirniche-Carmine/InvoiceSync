37318f1a03cb8a6140723e3ac3c4a10de27644ad
Carmine <potirnichecarmine@gmail.com>
2025-03-06 12:02:33 -0800

    Removed privacy

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,16 +1,11 @@
 "use client"
 
-import Link from 'next/link';
-
 export function Footer() {
   return (
     <footer className="w-full p-4 flex justify-center border-t border-gray-300">
-      <Link href='/policy' className= "flex-1 flex justify-center">
         <div className="">
           <p>&copy; 2024 Locksmith4U. All rights reserved.</p>
         </div>
-      </Link>
     </footer>
   );
 };
-
