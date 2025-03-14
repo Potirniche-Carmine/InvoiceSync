64a1e743fd7f8b663bb128f13bc8f4d0a2aef361
Carmine <potirnichecarmine@gmail.com>
2025-03-14 16:18:41 -0700

    Fixed it finally

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,15 @@
-import { getToken} from "next-auth/jwt";
+// src/middleware.ts
+import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 const secret = process.env.NEXTAUTH_SECRET;
 
-export default async function middleware(req: NextRequest){
-  const token = await getToken({req, secret});
+export default async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret });
   const isApiRoute = req.nextUrl.pathname.startsWith('/api/');
-
+  const isAuthRoute = req.nextUrl.pathname === '/';
+  
   if (!token) {
     if (isApiRoute) {
       return new NextResponse(
@@ -20,24 +22,33 @@ export default async function middleware(req: NextRequest){
         }
       );
     }
-    return NextResponse.redirect(new URL("/", req.nextUrl));
-  }
 
+    if (!isAuthRoute) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+    
+    return NextResponse.next();
+  }
+  
   const currentTime = Date.now();
   const loginTime = token.loginTimeStamp as number;
-
   const diffInMilliseconds = currentTime - loginTime;
-  const diffinHours = diffInMilliseconds / (1000 * 60 * 60);
-
-  if (diffinHours > 24) {
-    return NextResponse.redirect(new URL("/", req.nextUrl));
+  const diffInHours = diffInMilliseconds / (1000 * 60 * 60);
+  
+  if (diffInHours > 24) {
+    return NextResponse.redirect(new URL("/", req.url));
   }
+  
+  if (isAuthRoute) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+  
   return NextResponse.next();
-
-};
+}
 
 export const config = {
   matcher: [
+    "/",
     "/dashboard",
     "/dashboard/:path*",
     "/api/data/:path*",
